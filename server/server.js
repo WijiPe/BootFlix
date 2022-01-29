04a7f2e2db36cb22e333277484779c256b508f6f
Craig Brinkman <craigfrancis77@gmail.com>
2022-01-29 13:17:53 -0700

    commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+// IMPORT PACKAGES
+const express = require('express');
+const app = express();
+const cors = require('cors')
+port = 8000
+
+// CONFIG MONGOOSE
+// require("./config/mongoose.config");
+
+// CONFIG EXPRESS
+app.use(cors()) // Having 2 localhost port to communicate
+app.use(express.json(), express.urlencoded({ extended: true }))  // POST METHOD
+
+// ROUTES
+// require("./routes/BootFlix.routes")(app)
+
+// PORT
+app.listen(8000, () => console.log(`Listening on port: port`));
