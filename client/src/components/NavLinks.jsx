d9684b350562ed31cc82cba9d6517cec1e9a2c36
Wiji <peawseaw@gmail.com>
2022-01-29 14:58:33 -0800

    commit

--- client/src/components/NavLinks.jsx
+++ client/src/components/NavLinks.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import {Link} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
 import BootFlixLogo from './BootFlixLogo';
 import styles from './style.module.css'
 
 const NavLinks = () => {
+
+    const history = useHistory();
+
+    const onChangeHandler=(e) =>{
+        e.preventDefault()
+        history.push(e.target.value)
+    }
+
     return (
         <div>
             <div className={styles.nav}>
@@ -16,7 +24,11 @@ const NavLinks = () => {
                     <Link to={"/home"}>Kids</Link>
                 </div>
                 <div>
-                    
+                    <select type = "text" onChange={(e)=>onChangeHandler(e.target.value)}>
+                        <option value="">User</option>
+                        <option value="/edit/user/:id">Edit</option>
+                        <option value="">Log Out</option>
+                    </select>
                 </div>
             </div>
         </div>
