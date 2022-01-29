80f3f6c09b03b5d5ba5bf5ec571ff69e2774b821
Craig Brinkman <craigfrancis77@gmail.com>
2022-01-29 16:45:40 -0700

    commit

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,10 +1,23 @@
-const userController = require('../controllers/user.controller');
+const UserController = require("../controllers/user.controller");
+const {authenticate} = require("../config/jwt")
 
-module.exports = (app) => {
-    // app.get('/api', userController.index);
-    app.post('/api/user/create', userController.createUser)
-    app.get('/api/users', userController.getAllUsers)
-    app.get('/api/user/:id', userController.getUser)
-    app.put('/api/user/update/:id', userController.updateUser)
-    app.delete('/api/user/delete/:id', userController.deleteUser)
+
+module.exports = app=>{
+    app.post("/api/register", UserController.register);
+    app.post("/api/login", UserController.login);
+    app.get("/api/users/getloggedinuser", authenticate, UserController.getLoggedInUser);
 }
+
+
+
+
+// const userController = require('../controllers/user.controller');
+
+// module.exports = (app) => {
+//     // app.get('/api', userController.index);
+//     app.post('/api/user/create', userController.createUser)
+//     app.get('/api/users', userController.getAllUsers)
+//     app.get('/api/user/:id', userController.getUser)
+//     app.put('/api/user/update/:id', userController.updateUser)
+//     app.delete('/api/user/delete/:id', userController.deleteUser)
+// }
