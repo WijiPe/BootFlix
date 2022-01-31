821c43b889ef81fd924f5062f6ba5a7643bfde4c
Wiji <peawseaw@gmail.com>
2022-01-30 16:02:55 -0800

    commit

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -5,23 +5,37 @@ import axios from 'axios';
 const Home = () => {
 
       const [loggedinuser, setLoggedInUser] = useState({})
+      const [popular, setPopular] = useState([])
 
 
     useEffect(()=>{
         axios.get("http://localhost:8000/api/users/getloggedinuser", {withCredentials:true})
             .then(res=>{
                 console.log("logged in user info", res)
-                setLoggedInUser(res.data)
+                setLoggedInUser(res)
             })
             .catch(err=> {
                 console.log("errorrrrrr",err)
             })
     }, [])
 
+    useEffect(()=>{
+      axios.get("https://api.themoviedb.org/3/movie/popular?api_key=c49e028232019660cab8e28bf4d018d9&language=en-US&page=1")
+          .then(res=>{
+              console.log(res.data.results)
+              setPopular(res.data.results[0])
+          })
+          .catch(err=> {
+              console.log("errorrrrrr",err)
+          })
+  }, [])
+
   return (
     <div>
         <NavLinks  />
         <h1>Welcome, {loggedinuser.username}</h1>
+        <img src={popular.poster_path} alt="Poster" />
+        <p>{popular.title}</p>
     </div>
   )
 }
