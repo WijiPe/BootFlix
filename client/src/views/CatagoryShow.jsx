372c34cba1cbbf2044be86dc28091b05e665f1b4
Wiji <peawseaw@gmail.com>
2022-01-31 20:22:48 -0800

    commit

--- client/src/views/CatagoryShow.jsx
+++ client/src/views/CatagoryShow.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import NavLinks from '../components/NavLinks'
 import Popular from '../components/Popular'
+import Action from '../components/Action';
+import {useParams} from "react-router-dom";
 // import Catagory from '../components/Catagory'
 
 const CatagoryShow = () => {
 
     // const [catagory, setCatagory] = useState("")
+    const {catagory} = useParams()
 
     // const findCatagory =(newCatagory) =>{
     //     setCatagory(newCatagory)
@@ -15,7 +18,9 @@ const CatagoryShow = () => {
     return (
         <div>
             <NavLinks />
-            <Popular />
+            {catagory === "popular" && <Popular />}
+            {catagory === "action" && <Action />}
+
             {/* <h3><Catagory findCatagory={findCatagory} /></h3> */}
         </div>
     )
