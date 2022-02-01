8ac981d1e807d4fa2961b73b4bc83dbc17660587
Craig Brinkman <craigfrancis77@gmail.com>
2022-02-01 14:36:22 -0700

    index changes

--- client/src/views/Index.jsx
+++ client/src/views/Index.jsx
@@ -1,29 +1,61 @@
-import React from 'react';
+
 import BootFlixLogo from '../components/BootFlixLogo';
 import {useHistory} from "react-router-dom";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import ButtonGroup from '@mui/material/ButtonGroup';
 
-const Index = () => {
 
-    
+export default function Index() {
     const history = useHistory();
-
+    
+    const buttons = [
+        <Button onClick={e => history.push('/login')} key="one">Login</Button>,
+        <Button onClick={e => history.push('/signup')} key="two">Register</Button>
+    ];
+    
     const onChangeHandler=(e) =>{
         e.preventDefault()
         history.push(e.target.value)
     }
-
     return (
-        <div className=''>
-            <BootFlixLogo  />
-            <div>
-                    <select onChange={(e)=>onChangeHandler(e)}>
-                        <option type="hidden" value=""></option>
-                        <option value="/login">Log In</option>
-                        <option value="/signup">Sign Up</option>
-                    </select>
-                </div>
-        </div>
-    )
+    
+    <Box
+    sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        '& > *': {
+            mt: 16,
+            
+        },
+    }}
+    >
+    <BootFlixLogo  />
+    
+    <ButtonGroup size="large" color = "error" aria-label="medium error button group">
+        {buttons}
+    </ButtonGroup>
+
+    </Box>
+
+    
+);
 }
 
-export default Index
+//     return (
+//         <div className=''>
+            
+
+//             <div>
+//                     <select onChange={(e)=>onChangeHandler(e)}>
+//                         <option type="hidden" value=""></option>
+//                         <option value="/login">Log In</option>
+//                         <option value="/signup">Sign Up</option>
+//                     </select>
+//                 </div>
+//         </div>
+//     )
+// }
+
